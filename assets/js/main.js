1dd5b87e5e22b24f5206777425590c2c7a2ee55d
Duy Huỳnh <huynhlamduylcd@gmail.com>
2022-11-19 14:16:13 +0700

    add file to github

---
+++ assets/js/main.js
@@ -0,0 +1,55 @@
+/*===== MENU SHOW =====*/ 
+const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId),
+            nav = document.getElementById(navId)
+
+    if (toggle && nav ){
+        toggle.addEventListener('click',()=>{
+            nav.classList.toggle('show')
+        })
+    }
+}
+showMenu('nav-toggle','nav-menu')
+
+/*===== REMOVE MENU =====*/
+const navLink = document.querySelectorAll('.nav__link'),
+    navMenu = document.getElementById('nav-menu')
+
+function linkAction (){
+    navMenu.classList.remove('show')
+}
+
+navLink.forEach(n=>n.addEventListener('click', linkAction))  // chỗ này có thể bị lỗi, clip phút thứ 16:15
+
+
+/*===== SCROLL SECTIONS ACTIVE LINK =====*/
+const section = document.querySelectorAll('section[id]')
+
+window.addEventListener('scroll', scroolActive)
+
+function scroolActive(){
+    const scrollY = window.pageYOffset
+
+    section.forEach(current => {
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50
+        sectionId = current.getAttribute('id')
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('nav__menu a[href*=' + sectionId + ']'.classList.add(active))
+        }
+        else{
+            document.querySelector('nav__menu a[href*=' + sectionId + ']'.classList.add(active))
+            
+        }
+
+
+    })
+}
+
+/*===== CHANGE COLOR HEADER =====*/ 
+window.onscroll = ()=> {
+    const nav = document.getElementByI('header')
+    if (this.scrollY >= 200) nav.classList.add('scroll-header');
+    else nav.classList.remove('scroll-header');
+}
