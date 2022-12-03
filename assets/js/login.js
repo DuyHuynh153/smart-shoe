5f739b83514441c6468045208ffc0d834bd6c77b
AnhTuan176 <117256799+AnhTuan176@users.noreply.github.com>
2022-12-03 11:55:28 +0700

    Update login.js

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,31 +1,41 @@
-let username = document.getElementById("username");
-let email = document.getElementById("email");
-let password = document.getElementById("password");
+var username = document.getElementById("username");
+var password = document.getElementById("password");
 let btnLogin = document.querySelector(".btn-login");
 
 
 
 btnLogin.addEventListener("click", (e) => {
   e.preventDefault();
-  let user = {
-    username: username.value,
-    email: email.value,
-    password: password.value,
-  };
-  console.log(user);
-  let json = JSON.stringify(user);
-  if (!username.value || !email.value || !password.value) {
-    alert("vui long nhap day du thong tin");
-  }
-  if (localStorage.getItem(username.value) == json) {
-    alert("dang nhap thanh cong");
- 
-    window.location.href = "index.html";
+  var userArray = [];
+    userArray = JSON.parse(localStorage.getItem('taikhoan'));
+    if (!username.value || !password.value) {
+      alert("vui long nhap day du thong tin");
+    }
+    for(i=0;i<userArray.length;i++)
+		if(userArray[i].username==username.value && userArray[i].password==password.value)
+			{
+        alert("dang nhap thanh cong");
 
-    let signIn = document.getElementById("sign-in");
-    signIn.style.display = "none ! important";
-  } 
-  else {
-    alert("dang nhap that bai");
-  }
+        var userArray1 = [];
+	
+	if(localStorage.getItem('dangnhap')==null){		
+		var user1 = {username1:username.value , password1: password.value};
+		userArray1.push(user1);
+		console.log(userArray1);
+		localStorage.setItem('dangnhap',JSON.stringify(userArray1));
+	}
+
+        window.location="index.html";
+      }
+    else
+    {
+      alert("dang nhap khong thanh cong");
+    }
+});
+
+btnLogout.addEventListener("click", (e) => {
+  e.preventDefault();
+	var userArray = JSON.parse(localStorage.getItem('dangnhap'));
+	userArray.splice(i, 0);
+	localStorage.setItem('dangnhap',JSON.stringify(userArray));
 });
