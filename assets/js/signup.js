7bb9cc77d5c2d9ee79823319bfac2c6e1e13fb70
AnhTuan176 <117256799+AnhTuan176@users.noreply.github.com>
2022-12-03 11:54:48 +0700

    Update signup.js

--- assets/js/signup.js
+++ assets/js/signup.js
@@ -1,21 +1,33 @@
 let username = document.getElementById("username");
-let email = document.getElementById("email");
 let password = document.getElementById("password");
 let btnSignup = document.querySelector(".btn-signup");
 let btnLogin = document.querySelector(".btn-login");
 
 btnSignup.addEventListener("click", (e) => {
   e.preventDefault();
-  let user = {
-    username: username.value,
-    email: email.value,
-    password: password.value,
-  };
-  let json = JSON.stringify(user);
-  if (!username.value || !email.value || !password.value) {
+  var userArray = [];
+	
+	if(localStorage.getItem('taikhoan')==null){		
+		var user1 = {username: 'admin', password: 'admin'};
+		var user2={username:'guest1' , password: '123'};
+		userArray.push(user1);
+		userArray.push(user2);
+		console.log(userArray);
+		localStorage.setItem('taikhoan',JSON.stringify(userArray));
+	}
+  
+  if (!username.value || !password.value) {
     alert("vui long nhap day du thong tin");
   } else {
-    localStorage.setItem(username.value, json);
+    var user1;
+	userArray = JSON.parse(localStorage.getItem('taikhoan'));
+	user1 = {username: document.getElementById("username").value, password: document.getElementById("password").value};
+	console.log(user1.username);
+	console.log("Type user 1:");
+	console.log(typeof(user1));
+	userArray.push(user1);
+	
+	localStorage.setItem('taikhoan',JSON.stringify(userArray));
     alert("dang ky thanh cong");
   }
 });
