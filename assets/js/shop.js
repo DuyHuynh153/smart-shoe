13d01ec24fa6f69af63263df3a47d593e74d6d06
AnhTuan176 <117256799+AnhTuan176@users.noreply.github.com>
2022-12-07 14:38:40 +0700

    Update shop.js

--- assets/js/shop.js
+++ assets/js/shop.js
@@ -7,8 +7,28 @@ var giohang = JSON.parse(localStorage.getItem("giohang"));
 console.log("giohang")
 console.log(giohang);
 
+function thanhToanXong() {
+    alert("Thanh toan thanh cong");
+    var gHang = JSON.parse(localStorage.getItem('giohang'));
+    if(localStorage.getItem('donhang')!=null){	
+        var dHang = JSON.parse(localStorage.getItem('giohang'));	
+        dHang.push(gHang);
+        localStorage.setItem('donhang',JSON.stringify(gHang));
+      }	
+      else{localStorage.setItem("donhang",JSON.stringify(gHang));}
+      localStorage.removeItem("giohang");
+    window.location="shop.html";
+}
 
-
+var temp=0;
+function tongSoTien() {
+    var gHang = JSON.parse(localStorage.getItem('giohang'));
+    for (let index = 0; index < gHang.length; index++) {
+      temp+=gHang[index].price*document.getElementById("soLuong"+index).value;
+    }
+    document.getElementById("thanhTien").innerHTML="Thành Tiền: "+temp;
+    document.getElementById("tongTienThanhToan").innerHTML=temp;
+  }
 
 // --------------------------
 var html='';
@@ -17,7 +37,7 @@ for (i=0;i<giohang.length;i++){
         <img src="./assets/img/${giohang[i].image}" alt="" class="sneaker__img">
         <span class="sneaker__name shop-description__name ">${giohang[i].title}</span>
         <span class="sneaker__preci shop-description__price">${giohang[i].price}<span>
-        <input class="soLuongSp" type="number" name="" id="soLuong" placeholder="Số lượng">
+        <input class="soLuongSp" type="number" name="" id="soLuong${i}" placeholder="Số lượng">
 
     </article>`;
 
